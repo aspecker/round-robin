e54d617c260e271a00bc3facd5ce4d4c757c90b3
Adam Specker <specker@gmail.com>
2018-07-02 14:57:45 -0400

    components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = (props) =>(
-          
+   
     <button >{props.text} </button>
 
 )
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
-
 };
 
 export default Button;
