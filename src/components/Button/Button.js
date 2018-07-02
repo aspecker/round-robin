2c19a8820046bcb5860c1d9e6f39cd061cdc447c
Adam Specker <specker@gmail.com>
2018-07-02 14:06:24 -0400

    building components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './Button.css';
 
-const Button = (props) => (
-    <button >{props.text} </button>
+const Button = (props) =>(
+          
+    <Link to={props.url}>
+        <button >{props.text} </button>
+    </Link>
+
 )
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+};
 
 export default Button;
